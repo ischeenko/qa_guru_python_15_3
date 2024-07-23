58e80a83d25207bd50e1a8bc4644ae2853c8f4fe
Alexander Ischenko <ischeenko@gmail.com>
2024-07-23 14:24:20 +0300

    Add Selene to requirements.txt, delete commentary.

--- test_something.py
+++ test_something.py
@@ -1,10 +1,14 @@
 from selene import browser, be, have
 import pytest
 
+
 def test_first():
-    browser.open('https://ya.ru')
-    browser.element('[name="text"]').should(be.blank).type('olololo').press_enter()
-    browser.element('[id="search"]').should(have.text('pumpum'))
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('fgdfsgsdfgsfdgsfd1111').press_enter()
+    browser.element('[id="botstuff"]').should(be.not_.visible)
+
 
 def test_second():
-    browser.open('https://google.com')
+    browser.open('https://ya.ru')
+    browser.element('[name="text"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('.OrganicTitle ').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
